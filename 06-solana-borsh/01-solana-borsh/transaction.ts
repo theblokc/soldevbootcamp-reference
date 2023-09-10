4d120eca57de82ad2805c6579c8b66b1d2d546a1
kimerran <mh.neri@gmail.com>
2023-09-10 10:56:01 +0800

    updated borsh transaction program ID

--- 06-solana-borsh/01-solana-borsh/transaction.ts
+++ 06-solana-borsh/01-solana-borsh/transaction.ts
@@ -4,7 +4,7 @@ import { Movie } from './Movie.schema'
 import base58 from 'bs58'
 const keypairFromSecretKey = Web3.Keypair.fromSecretKey(base58.decode(process.env.SOL_PRIVATE_KEY as any))
 async function main() {
-    const MOVIE_REVIEW_PROGRAM_ID = 'MOVIE_PROGRAM_ID'
+    const MOVIE_REVIEW_PROGRAM_ID = 'M1w77y9CwjreHYSkRaQKKYVQKC9Pg15tESwnZkTxyVa'
     const movie = new Movie('The Matrix', 5, 'A movie about the matrix')
     const publicKey = new Web3.PublicKey('5Eh25gUPJDoNtTd3v6vpZ5VAwmjUUrxN28MpJDCfR2xA')
     const instruction = new Web3.TransactionInstruction({
